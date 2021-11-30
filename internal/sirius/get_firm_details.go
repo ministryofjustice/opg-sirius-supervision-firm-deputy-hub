7dcdb7e674055f080dbfef5236e6bc15c6857373
Kate <kate.smith@digital.justice.gov.uk>
2021-11-30 12:22:05 +0000

    sw-5278-works locally, breaks if no valid firm id and one server test;

---
+++ internal/sirius/get_firm_details.go
@@ -0,0 +1,69 @@
+package sirius
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type FirmDetails struct {
+	ID                               int                  `json:"id"`
+	FirmName                 		string             	  `json:"firmName"`
+	FirmNumber                 		int               	`json:"firmNumber"`
+	Email                            string               `json:"email"`
+	PhoneNumber                      string               `json:"phoneNumber"`
+	AddressLine1                     string               `json:"addressLine1"`
+	AddressLine2                     string               `json:"addressLine2"`
+	AddressLine3                     string               `json:"addressLine3"`
+	Town                             string               `json:"town"`
+	County                           string               `json:"county"`
+	Postcode                         string               `json:"postcode"`
+}
+
+
+func (c *Client) GetFirmDetails(ctx Context, firmId int) (FirmDetails, error) {
+	var v FirmDetails
+
+	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/firm/%d", firmId), nil)
+
+	fmt.Println("error")
+	fmt.Println(err)
+
+	if err != nil {
+		return v, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return v, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return v, ErrUnauthorized
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return v, newStatusError(resp)
+	}
+
+	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
+
+	if !statusOK {
+		var ve struct {
+			ValidationErrors ValidationErrors `json:"validation_errors"`
+		}
+
+		if err := json.NewDecoder(resp.Body).Decode(&v); err == nil {
+			return v, ValidationError{
+				Errors: ve.ValidationErrors,
+			}
+		}
+
+		return v, newStatusError(resp)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	return v, err
+}
