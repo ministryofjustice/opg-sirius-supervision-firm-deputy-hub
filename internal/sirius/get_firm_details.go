09c6c174dcbccb4c2e72308e834d406ade09ec74
Kate Smith <52196833+kate-49@users.noreply.github.com>
2021-12-09 08:16:50 +0000

    Sw 5282 pro firm details in hub (#8)
    * sw-5282 most of page working but missing some ui details
    
    * sw-5282 added and ran prettifier
    
    * sw-5282 added cypress tests and sirius tests
    
    * sw-5282 move port hosting to 8887 so can run alongside pro hub
    
    * sw-5282 fixed styling
    
    * prettified template and ensured cypress tests can run locally
    
    * sw-5282 remove placeholder template info
    
    * Update firm_deputy_hub.spec.js
    
    * Update firm-hub.gotmpl
    
    * sw-5282 refactored to remove calculate number of deputies
    
    * sw-5282 fix test

--- internal/sirius/get_firm_details.go
+++ internal/sirius/get_firm_details.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+type Deputy struct {
+	DeputyId int `json:"id"`
+	DeputyNumber int `json:"deputyNumber"`
+	OrganisationName string `json:"organisationName"`
+}
+
 type FirmDetails struct {
 	ID           int    `json:"id"`
 	FirmName     string `json:"firmName"`
@@ -18,6 +24,8 @@ type FirmDetails struct {
 	Town         string `json:"town"`
 	County       string `json:"county"`
 	Postcode     string `json:"postcode"`
+	Deputies     []Deputy `json:"deputies"`
+	TotalNumberOfDeputies int
 }
 
 func (c *Client) GetFirmDetails(ctx Context, firmId int) (FirmDetails, error) {
@@ -45,5 +53,7 @@ func (c *Client) GetFirmDetails(ctx Context, firmId int) (FirmDetails, error) {
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(&v)
+
 	return v, err
 }
+
