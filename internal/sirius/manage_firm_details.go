9dbe8f2329d1d90874730eb00f91b07fc390510b
Kate <kate.smith@digital.justice.gov.uk>
2022-01-07 11:56:19 +0000

    sw-5283 end of friday form loads autofilled

---
+++ internal/sirius/manage_firm_details.go
@@ -0,0 +1,67 @@
+package sirius
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (c *Client) ManageFirmDetails(ctx Context, amendedFirmDetails FirmDetails) error {
+	var k FirmDetails
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(FirmDetails{
+		ID:       amendedFirmDetails.ID,
+		FirmName:  amendedFirmDetails.FirmName,
+		Email: amendedFirmDetails.Email,
+		PhoneNumber:  amendedFirmDetails.PhoneNumber,
+		AddressLine1:    amendedFirmDetails.AddressLine1,
+		AddressLine2: amendedFirmDetails.AddressLine2,
+		AddressLine3:  amendedFirmDetails.AddressLine3,
+		Town:    amendedFirmDetails.Town,
+		County: amendedFirmDetails.County,
+		Postcode:  amendedFirmDetails.Postcode,
+	})
+	if err != nil {
+		return err
+	}
+
+	requestURL := fmt.Sprintf("/api/v1/firms/%d", amendedFirmDetails.ID)
+
+	req, err := c.newRequest(ctx, http.MethodPut, requestURL, &body)
+
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.http.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusUnauthorized {
+		return ErrUnauthorized
+	}
+
+	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
+
+	if !statusOK {
+		var v struct {
+			ValidationErrors ValidationErrors `json:"validation_errors"`
+		}
+
+		if err := json.NewDecoder(resp.Body).Decode(&v); err == nil {
+			return ValidationError{
+				Errors: v.ValidationErrors,
+			}
+		}
+
+		return newStatusError(resp)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&k)
+	return err
+}
