4a542e84dbe25ad68bda68f6745cce18ff964117
Kate Smith <52196833+kate-49@users.noreply.github.com>
2022-01-12 16:29:24 +0000

    Sw 5283 manage pro details firm hub (#27)
    * sw-5283 new form with autofilled data linked to homepage
    
    * sw-5283 end of friday form loads autofilled
    
    * sw-5823 form submits and redirects but not changed on sirius side
    
    * sw-5283 error messages showing
    
    * sw-5283 added in tests
    
    * sw-5283 added second error message and test
    
    * sw-5283 fix casing in error message
    
    * remove irrelevant test

---
+++ internal/server/manage_firm_details_test.go
@@ -0,0 +1,76 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/ministryofjustice/opg-sirius-supervision-firm-deputy-hub/internal/sirius"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockManageFirmDetailsInformation struct {
+	count       int
+	lastCtx     sirius.Context
+	err         error
+	firmDetails sirius.FirmDetails
+}
+
+func (m *mockManageFirmDetailsInformation) GetFirmDetails(ctx sirius.Context, firmId int) (sirius.FirmDetails, error) {
+	m.count += 1
+	m.lastCtx = ctx
+
+	return m.firmDetails, m.err
+}
+
+func (m *mockManageFirmDetailsInformation) ManageFirmDetails(ctx sirius.Context, firmDetails sirius.FirmDetails) error {
+	m.count += 1
+	m.lastCtx = ctx
+
+	return m.err
+}
+
+func TestManageFirmDetails(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockManageFirmDetailsInformation{}
+	template := &mockTemplates{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler := renderTemplateForManageFirmDetails(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+
+	assert.Nil(err)
+
+	resp := w.Result()
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	assert.Equal(1, client.count)
+
+	assert.Equal(1, template.count)
+	assert.Equal("page", template.lastName)
+}
+
+func TestPostManageFirm(t *testing.T) {
+	assert := assert.New(t)
+	client := &mockManageFirmDetailsInformation{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/123", strings.NewReader(""))
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	var returnedError error
+
+	testHandler := mux.NewRouter()
+	testHandler.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		returnedError = renderTemplateForManageFirmDetails(client, nil)(sirius.PermissionSet{}, w, r)
+	})
+
+	testHandler.ServeHTTP(w, r)
+	assert.Equal(returnedError, Redirect("/123?success=firmDetails"))
+}
+
