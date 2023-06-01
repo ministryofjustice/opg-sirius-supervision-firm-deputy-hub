27c16b563946cfedb591e0a59c830709754b61c9
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2023-06-01 10:17:32 +0100

    Fixed date formatting

--- internal/sirius/format_date_and_time.go
+++ internal/sirius/format_date_and_time.go
@@ -3,7 +3,7 @@ package sirius
 import "time"
 
 const DateTimeFormat string = "2006-01-02T15:04:05+00:00"
-const DateTimeDisplayFormat string = "01/02/2006"
+const DateTimeDisplayFormat string = "02/01/2006"
 
 func FormatDateAndTime(formatForDateTime string, dateString string, displayLayoutDateTime string) string {
 	if dateString == "" {
