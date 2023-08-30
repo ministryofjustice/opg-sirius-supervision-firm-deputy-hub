a6429d839df855d4b8bc5bc116152385ea4489b0
nicholassully <nicholas.sully@digital.justice.gov.uk>
2023-08-30 11:15:26 +0100

    sw-6757 remvoing comments #minor

--- cypress/e2e/accessibility.cy.js
+++ cypress/e2e/accessibility.cy.js
@@ -2,18 +2,6 @@ import "cypress-axe";
 import { TerminalLog } from "../support/e2e";
 import navTabs from "../fixtures/navigation.json"
 
-// describe("Accessibility", () => {
-//     before(() => {
-//         cy.visit("/supervision/deputies/firm/1");
-//         // cy.url().should('contain', '/deputies/firm/1')
-//         cy.injectAxe();
-//     });
-//
-//     it("Should have no accessibility violations",() => {
-//         cy.checkA11y(null, null, TerminalLog)
-//     });
-// });
-
 describe("Accessibility", () => {
     navTabs.forEach(([page, url]) =>
         it(`should render ${page} page accessibly`, () => {
