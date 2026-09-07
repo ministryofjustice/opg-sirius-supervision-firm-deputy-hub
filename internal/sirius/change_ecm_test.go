b84067f53fd2985d8f10250ff19ac3cf1fb2f58f
Kate Smith <kate.smith5@justice.gov.uk>
2026-09-07 16:43:39 +0100

    sfs-377 comment out some of other pacts

--- internal/sirius/change_ecm_test.go
+++ internal/sirius/change_ecm_test.go
@@ -2,7 +2,6 @@ package sirius
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -10,8 +9,6 @@ import (
 
 	"github.com/ministryofjustice/opg-sirius-supervision-firm-deputy-hub/internal/mocks"
 	"github.com/ministryofjustice/opg-sirius-supervision-firm-deputy-hub/internal/model"
-	"github.com/pact-foundation/pact-go/v2/consumer"
-	"github.com/pact-foundation/pact-go/v2/matchers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -94,43 +91,43 @@ func TestChangeECMReturnsUnauthorisedClientError(t *testing.T) {
 	assert.Equal(t, ErrUnauthorized, err)
 }
 
-func TestChangeECM_contract(t *testing.T) {
-	pact, err := consumer.NewV2Pact(consumer.MockHTTPProviderConfig{
-		Consumer: "sirius-supervision-firm-deputy-hub",
-		Provider: "sirius",
-		LogDir:   "../../logs",
-		PactDir:  "../../pacts",
-	})
-	assert.NoError(t, err)
-
-	err = pact.
-		AddInteraction().
-		Given("I am an allocations user").
-		UponReceiving("A request to change a firms ECM").
-		WithRequest(http.MethodPut, SupervisionAPIPath+"/v1/firms/1/ecm", func(b *consumer.V2RequestBuilder) {
-			b.Header("Content-Type", matchers.S("application/json"))
-			b.Header("OPG-Bypass-Membrane", matchers.S("1"))
-			b.Header("Accept", matchers.S("application/json"))
-			b.Header("X-XSRF-TOKEN", matchers.Like("abcde"))
-			b.JSONBody(matchers.MapMatcher{
-				"ecmId": matchers.Like(32),
-			})
-		}).
-		WillRespondWith(200, func(b *consumer.V2ResponseBuilder) {
-			b.Header("Content-Type", matchers.S("application/json"))
-			b.JSONBody(matchers.MapMatcher{
-				"id":       matchers.Like(1),
-				"firmName": matchers.Like("Example Firm"),
-				"executiveCaseManager": matchers.Like(map[string]any{
-					"id":          32,
-					"displayName": "Example ECM",
-				}),
-			})
-		}).
-		ExecuteTest(t, func(config consumer.MockServerConfig) error {
-			client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://%s:%d", config.Host, config.Port))
-			return client.ChangeECM(getContext(nil), ExecutiveCaseManagerOutgoing{EcmId: 32}, model.FirmDetails{ID: 1})
-		})
-
-	assert.NoError(t, err)
-}
+//func TestChangeECM_contract(t *testing.T) {
+//	pact, err := consumer.NewV2Pact(consumer.MockHTTPProviderConfig{
+//		Consumer: "sirius-supervision-firm-deputy-hub",
+//		Provider: "sirius",
+//		LogDir:   "../../logs",
+//		PactDir:  "../../pacts",
+//	})
+//	assert.NoError(t, err)
+//
+//	err = pact.
+//		AddInteraction().
+//		Given("I am an allocations user").
+//		UponReceiving("A request to change a firms ECM").
+//		WithRequest(http.MethodPut, SupervisionAPIPath+"/v1/firms/1/ecm", func(b *consumer.V2RequestBuilder) {
+//			b.Header("Content-Type", matchers.S("application/json"))
+//			b.Header("OPG-Bypass-Membrane", matchers.S("1"))
+//			b.Header("Accept", matchers.S("application/json"))
+//			b.Header("X-XSRF-TOKEN", matchers.Like("abcde"))
+//			b.JSONBody(matchers.MapMatcher{
+//				"ecmId": matchers.Like(32),
+//			})
+//		}).
+//		WillRespondWith(200, func(b *consumer.V2ResponseBuilder) {
+//			b.Header("Content-Type", matchers.S("application/json"))
+//			b.JSONBody(matchers.MapMatcher{
+//				"id":       matchers.Like(1),
+//				"firmName": matchers.Like("Example Firm"),
+//				"executiveCaseManager": matchers.Like(map[string]any{
+//					"id":          32,
+//					"displayName": "Example ECM",
+//				}),
+//			})
+//		}).
+//		ExecuteTest(t, func(config consumer.MockServerConfig) error {
+//			client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://%s:%d", config.Host, config.Port))
+//			return client.ChangeECM(getContext(nil), ExecutiveCaseManagerOutgoing{EcmId: 32}, model.FirmDetails{ID: 1})
+//		})
+//
+//	assert.NoError(t, err)
+//}
